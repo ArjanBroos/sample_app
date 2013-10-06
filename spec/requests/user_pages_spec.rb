c78bad8fe24b55d037727739e60ab82529b80cf6
Arjan Broos <arjanbroos@live.nl>
2013-10-06 18:40:36 +0200

    Finish signing in and out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,8 +6,8 @@ describe "UserPages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign Up') }
-    it { should have_title(full_title('Sign Up')) }
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 
   describe "profile page" do
@@ -31,7 +31,7 @@ describe "UserPages" do
       describe "after submission" do
         before { click_button submit }
 
-        it { should have_title('Sign Up') }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
       end
     end
@@ -43,6 +43,22 @@ describe "UserPages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
         expect { click_button submit }.to change(User, :count)
+      end
+
+      describe "after saving the user" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Welcome") }
       end
     end
     
